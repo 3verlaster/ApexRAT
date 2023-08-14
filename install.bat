b59da02271c81d6d0741a90ee7c9f0b9b81a58dc
3verlaster <96373998+3verlaster@users.noreply.github.com>
2023-08-14 15:40:10 +0300

    Add files via upload
    [+] connect.apex
    [+] install.bat (WIN)
    [+] stub.py (soon)

---
+++ install.bat
@@ -0,0 +1,15 @@
+REM #------------BY 3verlaster------------#
+echo Apex RAT  made with love by 3verlaster
+REM #----------START-INSTALLING-----------#
+echo [/] installation started.
+echo [*] installing library "Pillow" ...
+pip install Pillow
+echo [+] library "Pillow" installed.
+echo [*] installing library "customtkinter" ...
+pip install customtkinter
+echo [+] library "customtkinter" installed.
+echo [*] installing library "vidstream" ...
+pip install vidstream
+echo [+] library "vidstream" installed.
+echo [\] installation completed!
+REM #----------------END------------------#
